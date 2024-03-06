113ab0af7463b643fc7fc5e2ee631de280c49c3a
koley2000 <koleyz1965@gmail.com>
2024-03-06 19:07:45 +0530

    third commit

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -9,7 +9,7 @@ export default class NewsItem extends Component {
         <div className="card mb-3" style={{ width: "62rem" }}>
           <div className='row g-0'>
             <div className='col-md-3'>
-              <img src={imageUrl ? imageUrl : "https://www.vskills.in/certification/blog/wp-content/uploads/2015/01/structure-of-a-news-report.jpg"} className="card-img-top" alt="..." style={{ height: "14rem" }} />
+              <img src={imageUrl ? imageUrl : "https://www.vskills.in/certification/blog/wp-content/uploads/2015/01/structure-of-a-news-report.jpg"} className="card-img-top" alt="..." style={{ height: "16rem" }} />
             </div>
             <div className='col-md-8'>
               <div className="card-body">
